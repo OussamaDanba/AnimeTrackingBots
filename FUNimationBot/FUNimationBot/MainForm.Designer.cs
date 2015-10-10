ec33188c6b47d78cac218d4e7d5ff34789d0e9ac
Shadoxfix <Shadoxfix@gmail.com>
2015-10-10 13:07:40 +0200

    FUNimationBot: Implement design

--- FUNimationBot/FUNimationBot/MainForm.Designer.cs
+++ FUNimationBot/FUNimationBot/MainForm.Designer.cs
@@ -28,12 +28,262 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.StatusLabel = new System.Windows.Forms.Label();
+            this.ToggleStatusButton = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.ChosenDatabaseLabel = new System.Windows.Forms.Label();
+            this.ChooseDatabaseButton = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SubredditTextBox = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.RecentListBox = new System.Windows.Forms.ListBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.ErrorListBox = new System.Windows.Forms.ListBox();
+            this.ThreadsListBox = new System.Windows.Forms.ListBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.IPAddressTextBox = new System.Windows.Forms.TextBox();
+            this.PortTextBox = new System.Windows.Forms.TextBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(48, 16);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Status:";
+            // 
+            // StatusLabel
+            // 
+            this.StatusLabel.AutoSize = true;
+            this.StatusLabel.ForeColor = System.Drawing.Color.Red;
+            this.StatusLabel.Location = new System.Drawing.Point(12, 39);
+            this.StatusLabel.Name = "StatusLabel";
+            this.StatusLabel.Size = new System.Drawing.Size(62, 13);
+            this.StatusLabel.TabIndex = 1;
+            this.StatusLabel.Text = "Not running";
+            // 
+            // ToggleStatusButton
+            // 
+            this.ToggleStatusButton.Enabled = false;
+            this.ToggleStatusButton.Location = new System.Drawing.Point(12, 69);
+            this.ToggleStatusButton.Name = "ToggleStatusButton";
+            this.ToggleStatusButton.Size = new System.Drawing.Size(120, 25);
+            this.ToggleStatusButton.TabIndex = 2;
+            this.ToggleStatusButton.Text = "Start";
+            this.ToggleStatusButton.UseVisualStyleBackColor = true;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(162, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(114, 16);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Current database:";
+            // 
+            // ChosenDatabaseLabel
+            // 
+            this.ChosenDatabaseLabel.AutoSize = true;
+            this.ChosenDatabaseLabel.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.ChosenDatabaseLabel.Location = new System.Drawing.Point(162, 39);
+            this.ChosenDatabaseLabel.Name = "ChosenDatabaseLabel";
+            this.ChosenDatabaseLabel.Size = new System.Drawing.Size(33, 13);
+            this.ChosenDatabaseLabel.TabIndex = 4;
+            this.ChosenDatabaseLabel.Text = "None";
+            // 
+            // ChooseDatabaseButton
+            // 
+            this.ChooseDatabaseButton.Location = new System.Drawing.Point(162, 69);
+            this.ChooseDatabaseButton.Name = "ChooseDatabaseButton";
+            this.ChooseDatabaseButton.Size = new System.Drawing.Size(120, 25);
+            this.ChooseDatabaseButton.TabIndex = 5;
+            this.ChooseDatabaseButton.Text = "Choose database";
+            this.ChooseDatabaseButton.UseVisualStyleBackColor = true;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(312, 9);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(69, 16);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Subreddit:";
+            // 
+            // SubredditTextBox
+            // 
+            this.SubredditTextBox.Location = new System.Drawing.Point(315, 39);
+            this.SubredditTextBox.MaxLength = 20;
+            this.SubredditTextBox.Name = "SubredditTextBox";
+            this.SubredditTextBox.Size = new System.Drawing.Size(120, 20);
+            this.SubredditTextBox.TabIndex = 7;
+            this.SubredditTextBox.Text = "anime";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(12, 112);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(54, 16);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Recent:";
+            // 
+            // RecentListBox
+            // 
+            this.RecentListBox.FormattingEnabled = true;
+            this.RecentListBox.HorizontalScrollbar = true;
+            this.RecentListBox.Location = new System.Drawing.Point(12, 131);
+            this.RecentListBox.Name = "RecentListBox";
+            this.RecentListBox.Size = new System.Drawing.Size(297, 147);
+            this.RecentListBox.TabIndex = 9;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(312, 112);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(47, 16);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "Errors:";
+            // 
+            // ErrorListBox
+            // 
+            this.ErrorListBox.FormattingEnabled = true;
+            this.ErrorListBox.HorizontalScrollbar = true;
+            this.ErrorListBox.Location = new System.Drawing.Point(315, 131);
+            this.ErrorListBox.Name = "ErrorListBox";
+            this.ErrorListBox.Size = new System.Drawing.Size(236, 147);
+            this.ErrorListBox.TabIndex = 11;
+            // 
+            // ThreadsListBox
+            // 
+            this.ThreadsListBox.FormattingEnabled = true;
+            this.ThreadsListBox.HorizontalScrollbar = true;
+            this.ThreadsListBox.Location = new System.Drawing.Point(557, 92);
+            this.ThreadsListBox.Name = "ThreadsListBox";
+            this.ThreadsListBox.Size = new System.Drawing.Size(276, 186);
+            this.ThreadsListBox.TabIndex = 12;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(554, 73);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(96, 16);
+            this.label6.TabIndex = 13;
+            this.label6.Text = "Active threads:";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(554, 9);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(45, 16);
+            this.label7.TabIndex = 14;
+            this.label7.Text = "Proxy:";
+            // 
+            // IPAddressTextBox
+            // 
+            this.IPAddressTextBox.Location = new System.Drawing.Point(557, 39);
+            this.IPAddressTextBox.MaxLength = 20;
+            this.IPAddressTextBox.Name = "IPAddressTextBox";
+            this.IPAddressTextBox.Size = new System.Drawing.Size(120, 20);
+            this.IPAddressTextBox.TabIndex = 15;
+            // 
+            // PortTextBox
+            // 
+            this.PortTextBox.Location = new System.Drawing.Point(683, 39);
+            this.PortTextBox.MaxLength = 20;
+            this.PortTextBox.Name = "PortTextBox";
+            this.PortTextBox.Size = new System.Drawing.Size(120, 20);
+            this.PortTextBox.TabIndex = 16;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.label8.Location = new System.Drawing.Point(554, 25);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(58, 13);
+            this.label8.TabIndex = 17;
+            this.label8.Text = "IP Address";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.label9.Location = new System.Drawing.Point(680, 25);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(26, 13);
+            this.label9.TabIndex = 18;
+            this.label9.Text = "Port";
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(845, 290);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.PortTextBox);
+            this.Controls.Add(this.IPAddressTextBox);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.ThreadsListBox);
+            this.Controls.Add(this.ErrorListBox);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.RecentListBox);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.SubredditTextBox);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.ChooseDatabaseButton);
+            this.Controls.Add(this.ChosenDatabaseLabel);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.ToggleStatusButton);
+            this.Controls.Add(this.StatusLabel);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "MainForm";
+            this.Text = "FUNimationBot";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label StatusLabel;
+        private System.Windows.Forms.Button ToggleStatusButton;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label ChosenDatabaseLabel;
+        private System.Windows.Forms.Button ChooseDatabaseButton;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox SubredditTextBox;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.ListBox ThreadsListBox;
+        public System.Windows.Forms.ListBox RecentListBox;
+        public System.Windows.Forms.ListBox ErrorListBox;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox IPAddressTextBox;
+        private System.Windows.Forms.TextBox PortTextBox;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
     }
 }
 
