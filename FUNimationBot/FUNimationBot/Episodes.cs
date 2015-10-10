4b86ef8d361063325f1e028fcf20d6b60c99df30
Shadoxfix <Shadoxfix@gmail.com>
2015-10-10 23:23:00 +0200

    FUNimationBot: Specifiy JsonProperties so that variables can be renamed

--- FUNimationBot/FUNimationBot/Episodes.cs
+++ FUNimationBot/FUNimationBot/Episodes.cs
@@ -1,54 +1,137 @@
-﻿using System.Collections.Generic;
+﻿using Newtonsoft.Json;
+using System.Collections.Generic;
 
 // FUNimation JSON format
 namespace FUNimationBot
 {
     public class Episode
     {
-        public string asset_id { get; set; }
-        public string funimation_id { get; set; }
-        public string pubDate { get; set; }
-        public string rating { get; set; }
-        public string quality { get; set; }
-        public string language { get; set; }
-        public int duration { get; set; }
-        public string simulcast { get; set; }
-        public string closed_captioning { get; set; }
-        public string url { get; set; }
-        public string promo { get; set; }
-        public string show_name { get; set; }
-        public string popularity { get; set; }
-        public string title { get; set; }
-        public string description { get; set; }
-        public string sequence { get; set; }
-        public decimal? number { get; set; }
-        public string video_type { get; set; }
-        public string show_id { get; set; }
-        public string thumbnail { get; set; }
-        public string season_id { get; set; }
-        public string season_number { get; set; }
-        public string genre { get; set; }
-        public string releaseDate { get; set; }
-        public string thumbnail_url { get; set; }
-        public string thumbnail_small { get; set; }
-        public string thumbnail_medium { get; set; }
-        public string thumbnail_large { get; set; }
-        public string video_url { get; set; }
-        public string closed_caption_location { get; set; }
-        public List<string> aips { get; set; }
-        public string dub_sub { get; set; }
-        public string featured { get; set; }
-        public string highdef { get; set; }
-        public string has_subtitles { get; set; }
-        public int element_position { get; set; }
-        public string tv_or_move { get; set; }
-        public string rating_system { get; set; }
-        public int display_order { get; set; }
-        public string extended_title { get; set; }
+        [JsonProperty("asset_id")]
+        public string AssetId { get; set; }
+
+        [JsonProperty("funimation_id")]
+        public string FUNimationId { get; set; }
+
+        [JsonProperty("pubDate")]
+        public string PublicationDate { get; set; }
+
+        [JsonProperty("rating")]
+        public string Rating { get; set; }
+
+        [JsonProperty("quality")]
+        public string Quality { get; set; }
+
+        [JsonProperty("language")]
+        public string Language { get; set; }
+
+        [JsonProperty("duration")]
+        public int Duration { get; set; }
+
+        [JsonProperty("simulcast")]
+        public string Simulcast { get; set; }
+
+        [JsonProperty("closed_captioning")]
+        public string ClosedCaptioning { get; set; }
+
+        [JsonProperty("url")]
+        public string URL { get; set; }
+
+        [JsonProperty("promo")]
+        public string Promo { get; set; }
+
+        [JsonProperty("show_name")]
+        public string ShowName { get; set; }
+
+        [JsonProperty("popularity")]
+        public string Popularity { get; set; }
+
+        [JsonProperty("title")]
+        public string EpisodeTitle { get; set; }
+
+        [JsonProperty("description")]
+        public string EpisodeDescription { get; set; }
+
+        [JsonProperty("sequence")]
+        public string Sequence { get; set; }
+
+        [JsonProperty("number")]
+        public decimal? EpisodeNumber { get; set; }
+
+        [JsonProperty("video_type")]
+        public string VideoType { get; set; }
+
+        [JsonProperty("show_id")]
+        public string ShowId { get; set; }
+
+        [JsonProperty("thumbnail")]
+        public string Thumbnail { get; set; }
+
+        [JsonProperty("season_id")]
+        public string SeasonId { get; set; }
+
+        [JsonProperty("season_number")]
+        public string SeasonNumber { get; set; }
+
+        [JsonProperty("genre")]
+        public string Genre { get; set; }
+
+        [JsonProperty("releaseDate")]
+        public string ReleaseDate { get; set; }
+
+        [JsonProperty("thumbnail_url")]
+        public string ThumbnailUrl { get; set; }
+
+        [JsonProperty("thumbnail_small")]
+        public string ThumbnailSmall { get; set; }
+
+        [JsonProperty("thumbnail_medium")]
+        public string ThumbnailMedium { get; set; }
+
+        [JsonProperty("thumbnail_large")]
+        public string ThumbnailLarge { get; set; }
+
+        [JsonProperty("video_url")]
+        public string VideoURL { get; set; }
+
+        [JsonProperty("closed_caption_location")]
+        public string ClosedCaptionLocation { get; set; }
+
+        // Stands for Ad Interrupts?
+        [JsonProperty("aips")]
+        public List<string> Aips { get; set; }
+
+        [JsonProperty("dub_sub")]
+        public string DubSub { get; set; }
+
+        [JsonProperty("featured")]
+        public string Featured { get; set; }
+
+        [JsonProperty("highdef")]
+        public string HighDef { get; set; }
+
+        [JsonProperty("has_subtitles")]
+        public string HasSubtitles { get; set; }
+
+        [JsonProperty("element_position")]
+        public int ElementPosition { get; set; }
+
+        // Seems like a typo
+        [JsonProperty("tv_or_move")]
+        public string TVOrMovie { get; set; }
+
+        [JsonProperty("rating_system")]
+        public string RatingSystem { get; set; }
+
+        [JsonProperty("display_order")]
+        public int DisplayOrder { get; set; }
+
+        [JsonProperty("extended_title")]
+        public string ExtendedTitle { get; set; }
     }
 
     public class Episodes
     {
+        [JsonProperty("videos")]
         public List<Episode> EpisodesList { get; set; }
     }
 }
